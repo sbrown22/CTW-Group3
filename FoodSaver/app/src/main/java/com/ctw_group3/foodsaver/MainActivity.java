cb47bea52b9a97a35165bf8bb5d45b2087acdb66
Elijah <boczelijah@gmail.com>
2021-06-22 23:13:37 -0400

    add registration activity and simple layout

--- FoodSaver/app/src/main/java/com/ctw_group3/foodsaver/MainActivity.java
+++ FoodSaver/app/src/main/java/com/ctw_group3/foodsaver/MainActivity.java
@@ -2,7 +2,10 @@ package com.ctw_group3.foodsaver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void loadRegistration(View view) {
+        Intent intent = new Intent(this, Registration.class);
+        startActivity(intent);
     }
 }
