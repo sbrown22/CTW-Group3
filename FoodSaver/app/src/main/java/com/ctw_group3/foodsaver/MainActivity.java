231bf814e580497c6361f144e33310ab681f252a
sbrown22 <brownvase535@aol.com>
2021-06-29 21:53:21 -0400

    Added Submission to the food saver app

--- FoodSaver/app/src/main/java/com/ctw_group3/foodsaver/MainActivity.java
+++ FoodSaver/app/src/main/java/com/ctw_group3/foodsaver/MainActivity.java
@@ -29,4 +29,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Posts.class);
         startActivity(intent);
     }
+
+    public void loadSubmission(View view){
+        Intent intent = new Intent(this, Submission.class);
+        startActivity(intent);
+    }
 }
